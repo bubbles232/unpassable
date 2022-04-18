02e6d6dc7e9184f646199cb90e80ff6fb0a0871d
Vianne Schwaebe <Vivicoolcat1@users.noreply.github.com>
2022-04-18 13:35:54 -0700

    Update Chemistry.java

--- unpassable/src/unpassable/Chemistry.java
+++ unpassable/src/unpassable/Chemistry.java
@@ -11,14 +11,15 @@ import java.util.Scanner;
 public class Chemistry {
 	 ArrayList <String> questions = new ArrayList<String>();
 	 ArrayList <String> answers = new ArrayList<String>();
+	 ArrayList <String> key = new ArrayList<String>();
 	File myObj = new File("chemistry.txt");
-	
-   
+	File aKey = new File ("Chem ans.txt") ;  
 	public void scan() {
 	try {
 	      
 	      Scanner myReader = new Scanner(myObj);
-	      int counter=0;
+	      Scanner myReader2 = new Scanner(aKey);
+	      
 	  
 	      while (myReader.hasNextLine()) {
 	        String data = myReader.nextLine();
@@ -26,18 +27,23 @@ public class Chemistry {
 	      if(data.substring(0,1).equals("(")) {    	
 	        answers.add(data);
 	      }
-	      //lol
+	      
 	      else{
 	    	  questions.add(data);
 	      }
 	      }
+	      while (myReader2.hasNextLine()) {
+		        String data2 = myReader2.nextLine();
+         key.add(data2);
+		      }
+		      
+		     
+		      myReader.close();
+		      } catch (FileNotFoundException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }}
 	      
-	     
-	      myReader.close();
-	      } catch (FileNotFoundException e) {
-	      System.out.println("An error occurred.");
-	      e.printStackTrace();
-	    }}
 
 public ArrayList <String> sortQs (){
 	ArrayList <String> newQ=new ArrayList<String>();
@@ -55,5 +61,14 @@ public String [][] sortAs (){
 		}
 	}
 	return newA;
+}
+public int getCorrectAnsIn(int questionNum) {
+	int index=0;
+		for (int i = 0; i < sortAs()[questionNum].length; i++) {
+			if(sortAs()[questionNum][i].substring(0,3).equals(key.get(questionNum))) {
+				index=i;
+		}
+	}
+		return index;
 }
 }
